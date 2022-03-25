36243f4ac794b5b653c6de5224dc08f0ebabd8ad
Pablo <pablem08@gmail.com>
2022-03-25 00:18:01 -0300

    empezando citas de mascotas

---
+++ src/components/Formulario.js
@@ -0,0 +1,102 @@
+import React, { Fragment, useState } from 'react';
+import uuid from 'uuid/v4'
+const Forumulario = () => {
+
+    // Crear State de Citas
+    const [cita, actualizarCita] = useState({
+        mascota: '',
+        propietario: '',
+        fecha: '',
+        hora: '',
+        sintomas: ''
+    });
+    const [error, actualizarError] = useState(false)
+
+    // Funcion que se ejecuta cada que el usuario escribe en un input
+    const actualizarState = e => {
+        actualizarCita({
+            ...cita,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    // Extraer los valores
+    const { mascota, propietario, fecha, hora, sintomas } = cita;
+
+    // Cuando el usuario presiona agregar cita
+    const submitCita = e => {
+        e.preventDefault();
+
+        // Validar
+        if (mascota.trim() === '' || propietario.trim() === '' || fecha.trim() === '' || hora.trim() === '' || sintomas.trim() === '') {
+            actualizarError(true);
+            return;
+        }
+
+        // Eliminar el mensaje previo
+        actualizarError(false);
+        // Asignar un ID
+        cita.id = uuid();
+        console.log(cita);
+        // Crear una Cita
+
+        // Reiniciar el form
+    }
+    return (
+        <Fragment>
+            <h2>Crear Cita</h2>
+            {error ? <p className='alerta-error'>Todos los campos son obligatorios</p> : null}
+            <form
+                onSubmit={submitCita}
+            >
+                <label>Nombre Mascota</label>
+                <input
+                    type='text'
+                    name='mascota'
+                    className='u-full-width'
+                    placeholder='Nombre Mascota'
+                    onChange={actualizarState}
+                    value={mascota}
+                />
+                <label>Nombre Dueño</label>
+                <input
+                    type='text'
+                    name='propietario'
+                    className='u-full-width'
+                    placeholder='Nombre dueño de la mascota'
+                    onChange={actualizarState}
+                    value={propietario}
+                />
+                <label>Fecha</label>
+                <input
+                    type='date'
+                    name='fecha'
+                    className='u-full-width'
+                    onChange={actualizarState}
+                    value={fecha}
+                />
+                <label>Hora Alta</label>
+                <input
+                    type='time'
+                    name='hora'
+                    className='u-full-width'
+                    onChange={actualizarState}
+                    value={hora}
+                />
+                <label>Sintomas</label>
+                <textarea
+                    className='u-full-width'
+                    name='sintomas'
+                    onChange={actualizarState}
+                    value={sintomas}
+                ></textarea>
+                <button
+                    type='submit'
+                    className='u-full-width button-primary'
+                >Agregar Cita</button>
+            </form>
+        </Fragment>
+    );
+}
+
+export default Forumulario;
